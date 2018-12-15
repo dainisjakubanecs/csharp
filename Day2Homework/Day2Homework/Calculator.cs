b72d604f3e32b7005b3e527e8a30f3c1a48ff484
dainisjakubanecs <45880448+dainisjakubanecs@users.noreply.github.com>
2018-12-15 17:08:47 +0200

    Day2Homework

---
+++ Day2Homework/Day2Homework/Calculator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day2Homework
+{
+    class Calculator
+    {
+        double a;
+        String op;
+        double b;
+
+
+        public Calculator(double a, String op, double b)
+        {
+            this.a = a;
+            this.op = op;
+            this.b = b;
+        }
+        public double typeNumber()
+        {
+            Console.WriteLine("Ievadiet skaitli a");
+            String a = Console.ReadLine();
+
+
+            return Convert.ToDouble(a);
+        }
+
+        public double typeNumber1()
+        {
+            Console.WriteLine("Ievadiet skaitli b");
+            String b = Console.ReadLine();
+
+            return Convert.ToDouble(b);
+        }
+
+        public String operators()
+        {
+            Console.WriteLine("Ievadiet operatoru");
+            String a = Console.ReadLine();
+
+            return a;
+        }
+
+        public double result()
+        {
+            switch (op)
+            {
+                case "+":
+                    {
+                        return a + b;
+                    }
+                case "-":
+                    {
+                        return a - b;
+                    }
+                case "/":
+                    {
+                        if (b == 0)
+                        {
+                            Console.WriteLine("Nevar dalīt ar nulli!");
+                            return 0;
+                        }
+                        else
+                        {
+                            return a / b;
+                        }
+
+                    }
+                case "*":
+                    {
+                        return a * b;
+                    }
+            }
+            return -9999;
+        }
+
+    }
+}
---
+++ Day2Homework/Day2Homework/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Day2Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadiet skaitli a");
+            String a = Console.ReadLine();
+            double x = Convert.ToDouble(a);
+
+            Console.WriteLine("Ievadiet operatoru");
+            String oper = Console.ReadLine();
+
+            Console.WriteLine("Ievadiet skaitli b");
+            String b = Console.ReadLine();
+            double y = Convert.ToDouble(a);
+
+
+            Calculator calculator = new Calculator(x, oper, y);
+
+            //double skaitlis1 = calculator.typeNumber();
+            //double skaitlis2 = calculator.typeNumber();
+
+            Console.WriteLine("Rezultāts ir " + calculator.result());
+            Console.ReadLine();
+        }
+    }
+}
