b76f3ec12daa657fc45ab8b50219d7d9aca4124e
dainisjakubanecs <45880448+dainisjakubanecs@users.noreply.github.com>
2018-12-16 14:11:13 +0200

    Homework

---
+++ Homework/Homework/Calculator.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+    class Calculator
+    {
+        double a;
+        String op;
+        double b;
+        String c;
+        String d;
+        String e;
+
+
+        public Calculator()
+        {           
+            //this.a = a;
+            //this.op = op;
+            //this.b = b;
+        }
+        /*public double typeNumber()
+        {
+            Console.WriteLine("Ievadiet skaitli a");
+            String a = Console.ReadLine();
+
+
+            return Convert.ToDouble(a);
+        }
+
+        public double typeNumber1()
+        {
+            Console.WriteLine("Ievadiet skaitli b");
+            String b = Console.ReadLine();
+
+            return Convert.ToDouble(b);
+        }
+
+        public String operators()
+        {
+            Console.WriteLine("Ievadiet operatoru");
+            String a = Console.ReadLine();
+
+            return a;
+        }
+        */
+
+        public double result()
+        {
+            Console.WriteLine("Ievadiet skaitli a");
+            c = Console.ReadLine();
+            a = Convert.ToDouble(c);
+
+            Console.WriteLine("Ievadiet operatoru");
+            op = Console.ReadLine();
+
+            Console.WriteLine("Ievadiet skaitli b");
+            d = Console.ReadLine();
+            b = Convert.ToDouble(d);
+            //e = Console.ReadLine();
+
+            switch (op)
+            {
+                case "+":
+                    {
+                        return a + b;
+                    }
+                case "-":
+                    {
+                        return a - b;
+                    }
+                case "/":
+                    {
+                        if (b == 0)
+                        {                        
+                            return 0;
+                        }
+                        else
+                        {
+
+                            return a / b;
+                        }
+
+                    }
+                case "*":
+                    {
+                        return a * b;
+                    }
+                case "^":
+                    {
+                        double temp = 1;
+                        for (double i = 0; i < b; i++)
+                        {
+
+                            temp = a * temp;
+                        }
+                        return temp;
+                    }
+            }
+            return -9999;
+        }
+
+        public void result2()
+        {           
+            while (e != "exit")
+            {
+                if (result() != (double)0)
+                {
+                    Console.WriteLine("Rezultāts ir " + result());
+                    
+                    e = Console.ReadLine();                
+                }                             
+                else
+                {
+                    Console.WriteLine("Nevar dalīt ar nulli!");
+                    e = Console.ReadLine();              
+                }               
+            }            
+        }
+    }
+}
+
---
+++ Homework/Homework/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Calculator calculator = new Calculator();
+           
+            calculator.result2();
+            
+            Console.ReadLine();
+        }
+    }
+}
